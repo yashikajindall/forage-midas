071d2359797dc9ff44f0c428b12967c068be298f
yashikajindal <c5013343@newcastle.ac.uk>
2026-03-30 02:36:38 +0100

    Feat: Add user lookup by name for debugging and test inspection

--- src/main/java/com/jpmc/midascore/repository/UserRepository.java
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserRecord, Long> {
     UserRecord findById(long id);
+    UserRecord findByName(String name);
 }
